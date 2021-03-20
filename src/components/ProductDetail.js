ab24e4a69a2c2c895d9073363d56f617f87021e7
yichun Lee <yichunlee@yichunde-MBP-2.Home>
2021-03-20 19:36:38 +0800

    wk4

---
+++ src/components/ProductDetail.js
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import { Row, Col } from "antd";
+import { Select } from 'antd';
+import AddToCart from "./AddToCart"
+
+const { Option } = Select;
+
+function ProductDetail({ product }) {
+   const [qty, setQty] = useState(product.countInStock > 0 ? 1 : 0);
+   const [color, setColor] = useState(product.countInStock > 0 ? product.color[0] : 0);
+ 
+   return (
+      <Row gutter={[32, 32]}>
+        <Col 
+          lg={{ span: 8, offset: 2 }}
+        >
+         <img
+            alt=""
+            className="product-image"
+            src={product.image}
+         />           
+        </Col>
+        <Col 
+          lg={{ span: 12 }}
+        >
+         <div className="product-info--detail">
+            <h2 className="product-category">
+               {product.category}
+            </h2>
+            <h1 className="product-name product-name--large">
+               {product.name}
+            </h1>
+            <p className="product-description">{product.description_long}</p>
+            <div className="product-price-wrap">
+               <p className="product-price product-price--large">
+                  US${product.price}.00
+               </p>
+               <p className="product-status">
+                  Status: {product.countInStock > 0 ? "In Stock" : "Unavailable."}
+               </p>
+              
+          <div className="product-buyinfo">
+               <p className="product-qty">
+                  Qty: {"   "}
+                  <Select 
+                     defaultValue={qty} 
+                     className="select-style"
+                     onChange={val=>setQty(val)}
+                  >
+                     {[...Array(product.countInStock).keys()].map((x) => (
+                        <Option key={x + 1} value={x + 1}>
+                           {x + 1}
+                        </Option>
+                     ))}
+                  </Select>
+               </p>
+               <p className="product-color">
+                     Color: {"   "}
+                     <Select 
+                        defaultValue={color} 
+                        className="select-style"
+                        onChange={val=>setColor(val)}
+                     >
+                        {[...Array(product.color.length).keys()].map((x) => (
+                           <Option value={product.color[x]}>
+                              {product.color[x]}
+                           </Option>
+                        ))}
+                     </Select>
+                  </p>
+            </div>
+            
+                <p className="product-qty">
+                  Total Price: {product.price * qty}
+                </p> 
+                <p className="product-color">
+                     Color: {color}
+                </p>
+                           
+               <AddToCart />
+            </div>
+         </div>           
+        </Col>
+      </Row>
+   );
+}
+
+export default ProductDetail;
